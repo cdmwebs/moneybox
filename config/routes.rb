4d714695552f23e787e5e54c99646b75bb03b92e
Warren Harrison <warren@hungry-media.com>
2013-01-22 06:44:59 -0500

    Add nested routes to accounts and envelopes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Moneybox::Application.routes.draw do
 
+  resources :envelopes, :accounts do
+    member do
+      resources :transactions
+    end
+  end
+
   resources :transactions, :envelopes, :accounts
 
   root to: 'transactions#index'
