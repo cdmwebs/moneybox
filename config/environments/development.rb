0593c28437a5f9829f4de278e229488372aec323
Warren Harrison <warren@helloample.com>
2013-02-10 21:25:09 -0500

    Add attachmentts to transactions. #29

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,15 +36,7 @@ Moneybox::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'hm-moneybox.herokuapp.com' }
-  ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
-    :port           => "25",
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV['SENDGRID_DOMAIN']
-  }
+  config.action_mailer.default_url_options = { :host => 'moneybox.dev' }
   config.action_mailer.delivery_method = :file
 
 end
