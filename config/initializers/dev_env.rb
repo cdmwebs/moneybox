abf2abd207cbf876bc4a45f0518248a5bdf1324e
Warren Harrison <warren@hungry-media.com>
2013-05-25 13:09:07 -0400

    Remove deployment-specific data

--- config/initializers/dev_env.rb
+++
@@ -1,8 +0,0 @@
-unless Rails.env.production?
-  ENV['SENDGRID_USERNAME'] = 'WarrenHarrison'
-  ENV['SENDGRID_PASSWORD'] = '5!3Sstw!afs7A4'
-  ENV['SENDGRID_DOMAIN'] = 'moneybox.dev'
-  ENV['AWS_ACCESS_KEY_ID'] = 'AKIAJ54NTHCZ3C7R2NSQ'
-  ENV['AWS_SECRET_ACCESS_KEY'] = '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'
-  ENV['S3_BUCKET_NAME'] = 'moneybox-dev'
-end
